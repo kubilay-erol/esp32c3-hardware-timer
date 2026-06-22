917d5b3584d17159934be2a770813753feac400b
Kubilay Erol <kubilay-erol@outlook.com>
2026-06-22 22:04:58 +0300

    Refactor timer methods and update timer retrieval

--- esp32c3-hardware-timer.cpp
+++ esp32c3-hardware-timer.cpp
@@ -1,5 +1,6 @@
 
-#include<cstdint>
+#pragma once
+#include <cstdint>
 
 constexpr uint64_t num = 3;
 
@@ -9,19 +10,23 @@ public:
     uint64_t reference[num];
 
     uint64_t get_timer() {
+    
+        *(reinterpret_cast<volatile uint32_t*>(0x60023004)) = (1 << 30);   
 
-
-        *(reinterpret_cast<volatile uint32_t*>(0x6001F00C)) = 1;   //write anything on latch  
-
-
-        uint32_t low = *(reinterpret_cast<volatile uint32_t*>(0x6001F004)); //low 
-       
-        uint32_t high = *(reinterpret_cast<volatile uint32_t*>(0x6001F008)); //high
+   
+        while ((*(reinterpret_cast<volatile uint32_t*>(0x60023004)) & (1 << 29)) == 0) {
         
+        }
 
-        uint64_t microseconds = (static_cast<uint64_t>(high) << 32) | low;
+    
+        uint32_t low = *(reinterpret_cast<volatile uint32_t*>(0x60023044)); 
+        uint32_t high = *(reinterpret_cast<volatile uint32_t*>(0x60023040)); 
 
-        return microseconds; 
+    
+        uint64_t ticks = (static_cast<uint64_t>(high) << 32) | low;
+
+        return ticks;
+    
     }
 
     uint64_t get_seconds(uint64_t micsec) {
@@ -33,7 +38,7 @@ public:
     }      
 
     uint64_t get_time(uint64_t n) {
-        return get_seconds((get_timer() - reference[n]));
+        return (get_timer() - reference[n]);
     }
 
     bool is_delay_complete(uint64_t delay_seconds, uint64_t n) {
